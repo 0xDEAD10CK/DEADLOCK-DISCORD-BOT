6c6618ce7be6a6b250d757cc35eb3f98f4df6cbc
JoshMBN <joshua.brennan.niu@gmail.com>
2025-02-15 01:46:34 +1300

    Enhance RecommendCog to send user recommendations to the bot owner; update on_ready message

--- src/cogs/opt_recommend_cog.py
+++ src/cogs/opt_recommend_cog.py
@@ -1,17 +1,25 @@
 import discord
 from discord.ext import commands
+import os
 
 class RecommendCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.owner_id = int(os.getenv('OWNER_ID'))
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f'[ WORKING ] - Raffle Cog has loaded!')
+        print(f'[ WORKING ] - Recommendation Cog has loaded!')
 
     @commands.command(name='recommend')
-    async def recommend(self, ctx):
-        await ctx.send('Hello! I am a Discord bot cog!')
+    async def recommend(self, ctx, *, message_content: str):
+        '''Recommend command for getting recommendations.'''
+        owner = await self.bot.fetch_user(self.owner_id)
+
+        print(ctx.message)
+
+        # Send owner a message
+        await owner.send(f'{ctx.author} has requested a recommendation!\n\nMessage: {message_content}')
 
 async def setup(bot):
     await bot.add_cog(RecommendCog(bot))
