7d2687c092bfba34c1850d2a1ad19d39d175034b
0xDEAD10CK <joshua.brennan.niu@gmail.com>
2025-03-17 04:20:33 +0000

    Made changes to Welcome Cog

--- src/cogs/opt_welcome_cog.py
+++ src/cogs/opt_welcome_cog.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import random
 import os
-from src.utils.welcome_messages import welcome_messages
+from utils.welcome_messages import welcome_messages
 
 class WelcomeCog(commands.Cog):
     def __init__(self, bot):
